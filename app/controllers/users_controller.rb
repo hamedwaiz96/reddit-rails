6e4068e8ca954638b2a440c7c547655cba3c6494
Hamed Waiz <hamedw96@mgmail.com>
2019-05-29 23:24:55 -0700

    Add Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,13 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    user = User.new(user_params)
+    if user.save
+      redirect_to users_url
+    else
+      flash.now[:errors] = ["Invalid Password or Username"]
+      render :new
+    end
   end
 
   def user_params
